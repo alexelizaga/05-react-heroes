5dc972a257e4fb5c95441e5a01f50d14c4cca2e9
alexelizaga <alexelizaga@icloud.com>
2022-05-09 15:59:21 +0200

    174

--- src/components/ui/MyNavbar.js
+++ src/components/ui/MyNavbar.js
@@ -1,24 +1,45 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 
 export const MyNavbar = () => {
+  const { pathname } = useLocation();
+
+  const handleLogout = () => {
+    // TODO: 
+    console.log('logout')
+  };
+
+  
+
   return (
     <Navbar bg='dark' variant='dark' expand='sm'>
       <Container>
         <Navbar.Brand as={Link} to="/">Asociaciones</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/marvel">Marvel</Nav.Link>
-            <Nav.Link as={Link} to="/dc">DC</Nav.Link>
+          <Nav activeKey={pathname} className="me-auto">
+            <Nav.Link
+              as={Link}
+              to='/marvel'
+              eventKey='/marvel'
+            >
+              Marvel
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to='/dc'
+              eventKey='/dc'
+            >
+              DC
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
 
         <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>Alex</Navbar.Text>
           <Nav>
-            <Nav.Link as={Link} to="/login">Logout</Nav.Link>
+            <Button variant='light' onClick={handleLogout}>Logout</Button>
           </Nav>
-          
         </Navbar.Collapse>
       </Container>
     </Navbar>
