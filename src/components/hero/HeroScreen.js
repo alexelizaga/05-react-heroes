0d07c2e8e63554c31751b0cb63a05333fa058fa0
alexelizaga <alexelizaga@icloud.com>
2022-05-10 11:06:43 +0200

    179

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,7 +1,21 @@
+import { useParams, Navigate } from 'react-router-dom';
+
+import { getHeroesById } from '../../selectors/getHeroById';
+
 export const HeroScreen = () => {
+  const { heroId } = useParams();
+  const hero = getHeroesById(heroId);
+
+  if (!hero) {
+    return <Navigate to='/' />
+  }
+
   return (
     <div>
       <h1>HeroScreen</h1>
+      <p>
+        {hero.superhero}
+      </p>
     </div>
   )
 }
